d93b5505fed5ce90947fb02d00bf1cc5b92c13fd
andreiligo <73296835+andreiligo@users.noreply.github.com>
2020-10-24 21:50:29 +0300

    Delete les1.py

--- homeworks/lesson1/les1.py
+++
@@ -1,8 +0,0 @@
-first_variable = "HELLO' GB"
-first_variable2 = 'Hello" \'Gb'
-
-print(first_variable)
-print(first_variable2)
-
-user_name = input('ваше имя\n >>>')
-print('Hello {}'.format(user_name))
