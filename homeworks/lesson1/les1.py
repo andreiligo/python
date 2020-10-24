65741b145daeee047da6fde113a37120c939e628
Andrei Ligo <lygolife@gmail.com>
2020-10-24 19:44:55 +0300

    Done

---
+++ homeworks/lesson1/les1.py
@@ -0,0 +1,8 @@
+first_variable = "HELLO' GB"
+first_variable2 = 'Hello" \'Gb'
+
+print(first_variable)
+print(first_variable2)
+
+user_name = input('ваше имя\n >>>')
+print('Hello {}'.format(user_name))
