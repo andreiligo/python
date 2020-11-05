d5956c8dd59bc5f238c4263ccc8ba9d77d4b788a
Andrei Ligo <lygolife@gmail.com>
2020-11-05 16:36:22 +0300

    Done

--- homeworks/lesson4/task7.py
+++ homeworks/lesson4/task7.py
@@ -1,6 +1,17 @@
-import itertools
+from itertools import count
+from math import factorial
 
-def func(n):
+
+def gen_func():
+    for el in count(1):
+        yield factorial(el)
 
 
-######
+var_gen = gen_func()
+x = 0
+for i in var_gen:
+    if x < 15:
+        print(i)
+        x += 1
+    else:
+        break
